de9ba51e4c351ec6a72d51d77c876569bbcd1ee6
Gunny556 <kan38774606@gmail.com>
2025-12-12 23:48:10 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int num;
+    int even_count = 0;
+    int odd_count = 0;
+
+    do {
+        cout << "Enter an integer: ";
+        cin >> num;
+        if (num == 0) break;    // หยุดรับข้อมูลถ้า user ใส่ 0
+        if (num % 2 == 0)
+            even_count++;
+        else
+            odd_count++;
+    } while (true);
+
+    cout << "#Even numbers = " << even_count << endl;
+    cout << "#Odd numbers = " << odd_count << endl;
     return 0;
 }
